a2d43db2caee90a66623b700416b82a2a8c93429
juniuszhou <junius.zhou@gmail.com>
2014-09-03 13:30:29 +0800

    init add

---
+++ hello.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+static double high;
+static double length;
+
+struct Node{
+	pair<double, double> dot;
+	double radius;
+	Node * next;
+}
+	
+class connected{
+	//to record if interconnect with frame.
+	pair<double, double> right;
+	pair<double, double> left;
+	pair<double, double> top;
+	pair<double, double> bottom;
+	
+	Node * head;	 
+}
+
+void interconnect(pair<double, double> dot, double radius){
+}
+
+bool escape(int w, int h, int n, double[] x, double[] y, double[] r)
+
+int main(){
+	cout << "hello world" << endl;
+}
